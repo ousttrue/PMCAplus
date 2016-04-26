8f70fc0155ee477420f8d037b7d478dda546cbd2
ousttrue <ousttrue@gmail.com>
2016-04-26 22:59:39 +0900

    remove print_PMD

--- sources/mPMD_rw.h
+++ sources/mPMD_rw.h
@@ -1,9 +1,5 @@
 #pragma once
 #include "mPMD.h"
 
-
-
 int load_PMD(MODEL *model, const char file_name[]);
 int write_PMD(MODEL *model, const char file_name[]);
-int print_PMD(MODEL *model, const char file_name[]);
-
