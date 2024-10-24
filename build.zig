c197cfd22c03a9a6b8855d1c49f3d986886f98dd
ousttrue <ousttrue@gmail.com>
2024-10-24 19:25:51 +0900

    add sokol-zig

--- build.zig
+++ build.zig
@@ -12,14 +12,14 @@ pub fn build(b: *std.Build) void {
         .name = "PMCA",
         .link_libc = true,
     });
-    const install = b.addInstallArtifact(dll, .{
+    const install_dll = b.addInstallArtifact(dll, .{
         .dest_sub_path = "PMCA.pyd",
     });
-    b.getInstallStep().dependOn(&install.step);
+    b.getInstallStep().dependOn(&install_dll.step);
     targets.append(dll) catch @panic("OOM");
 
     dll.addCSourceFiles(.{
-        .root = b.path("sources"),
+        .root = b.path("src"),
         .files = &.{
             "PMCA_PyMod.c",
             "PMCA_SDLMod.c",
@@ -53,6 +53,47 @@ pub fn build(b: *std.Build) void {
     dll.linkSystemLibrary("WINMM");
     dll.linkSystemLibrary("OPENGL32");
     dll.linkSystemLibrary("GLU32");
+
+    const exe = b.addExecutable(.{
+        .target = target,
+        .optimize = optimize,
+        .name = "pmcaz",
+        .root_source_file = b.path("src/main.zig"),
+    });
+    const install_exe = b.addInstallArtifact(exe, .{});
+    b.getInstallStep().dependOn(&install_exe.step);
+
+    const run = b.addRunArtifact(exe);
+    run.step.dependOn(&install_exe.step);
+
+    b.step("run", "run pmcaz").dependOn(&run.step);
+
+    const sokol_dep = b.dependency("sokol", .{
+        .target = target,
+        .optimize = optimize,
+        .with_sokol_imgui = true,
+    });
+    exe.root_module.addImport("sokol", sokol_dep.module("sokol"));
+
+    const cimgui_dep = b.dependency("cimgui", .{
+        .target = target,
+        .optimize = optimize,
+    });
+    // inject the cimgui header search path into the sokol C library compile step
+    const cimgui_root = cimgui_dep.namedWriteFiles("cimgui").getDirectory();
+    sokol_dep.artifact("sokol_clib").addIncludePath(cimgui_root);
+    sokol_dep.artifact("sokol_clib").addCSourceFile(.{ .file = b.path("deps/cimgui//custom_button_behaviour.cpp") });
+    exe.root_module.addImport("cimgui", cimgui_dep.module("cimgui"));
+
+    const rowmath_dep = b.dependency("rowmath", .{});
+    const rowmath = rowmath_dep.module("rowmath");
+    exe.root_module.addImport("rowmath", rowmath);
+
+    const stbi_dep = b.dependency("stb", .{
+        .target = target,
+        .optimize = optimize,
+    });
+    exe.root_module.addImport("stb", &stbi_dep.artifact("stb").root_module);
 
     zcc.createStep(b, "cdb", targets.toOwnedSlice() catch @panic("OOM"));
 }
