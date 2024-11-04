394006569b301861fe4dc4d942ed2e7e611a46dd
ousttrue <ousttrue@gmail.com>
2024-11-05 02:20:49 +0900

    PMCA_view.h

--- src/PMCA_view.c
+++ src/PMCA_view.c
@@ -1,16 +1,55 @@
 #include "PMCA_view.h"
 #include "dbg.h"
 #include "mlib_PMD_rw01.h"
+#include <Windows.h>
+
 #include <GL/GL.h>
 #include <GL/GLU.h>
 #include <SDL.h>
-#include <Windows.h>
 #include <math.h>
 #include <stb_image.h>
 
 #define SCALE (2.0 * 3.14159265358979323846)
 #define WM_TITLE "PMCA 3D View"
-SDL_Thread *viewer_th;
+
+static SDL_Thread *viewer_th = nullptr;
+
+/*
+** クォータニオンの積 r <- p x q
+*/
+static void qmul(double r[], const double p[], const double q[]) {
+  r[0] = p[0] * q[0] - p[1] * q[1] - p[2] * q[2] - p[3] * q[3];
+  r[1] = p[0] * q[1] + p[1] * q[0] + p[2] * q[3] - p[3] * q[2];
+  r[2] = p[0] * q[2] - p[1] * q[3] + p[2] * q[0] + p[3] * q[1];
+  r[3] = p[0] * q[3] + p[1] * q[2] - p[2] * q[1] + p[3] * q[0];
+}
+
+/*
+** 回転の変換行列 r <- クォータニオン q
+*/
+static void qrot(double r[], double q[]) {
+  double x2 = q[1] * q[1] * 2.0;
+  double y2 = q[2] * q[2] * 2.0;
+  double z2 = q[3] * q[3] * 2.0;
+  double xy = q[1] * q[2] * 2.0;
+  double yz = q[2] * q[3] * 2.0;
+  double zx = q[3] * q[1] * 2.0;
+  double xw = q[1] * q[0] * 2.0;
+  double yw = q[2] * q[0] * 2.0;
+  double zw = q[3] * q[0] * 2.0;
+
+  r[0] = 1.0 - y2 - z2;
+  r[1] = xy + zw;
+  r[2] = zx - yw;
+  r[4] = xy - zw;
+  r[5] = 1.0 - z2 - x2;
+  r[6] = yz + xw;
+  r[8] = zx + yw;
+  r[9] = yz - xw;
+  r[10] = 1.0 - x2 - y2;
+  r[3] = r[7] = r[11] = r[12] = r[13] = r[14] = 0.0;
+  r[15] = 1.0;
+}
 
 struct DSP_MAT {
   float col[4];
@@ -27,6 +66,14 @@ struct DSP_MODEL {
   int mats_c;
   struct DSP_MAT *mats;
   unsigned int *texid;
+};
+
+struct FLAGS {
+  int button1;
+  int button2;
+  int button3;
+  int model_lock;
+  int quit;
 };
 
 struct FLAGS myflags;
@@ -57,77 +104,12 @@ struct VIEW_STATE {
 
 struct VIEW_STATE vs;
 
-static int createwindow();
-static int setup_opengl(int width, int height);
-static void draw_screen(void);
-static void handle_key_down(SDL_keysym *keysym);
-static void process_events(void);
-
 /* 現在のビデオ設定についての情報 */
 const SDL_VideoInfo *info = NULL;
 /* ウィンドウの色のピクセル深度 */
 int bpp = 0;
 /* SDL_SetVideoMode に渡すフラグ */
 int flags = 0;
-
-// int (SDLCALL *fn)(void *)
-int viewer_thread(void *) {
-  SDL_Event event;
-
-  myflags.model_lock = 0;
-
-  createwindow();
-  SDL_Quit();
-  return 0;
-}
-
-static int createwindow() {
-  if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-    fprintf(stderr, "ビデオの初期化に失敗しました: %s\n", SDL_GetError());
-    SDL_Quit();
-    return 1;
-  }
-
-  info = SDL_GetVideoInfo();
-
-  if (!info) {
-    fprintf(stderr, "ビデオの問い合わせに失敗しました: %s\n", SDL_GetError());
-    SDL_Quit();
-    return 1;
-  }
-
-  vs.width = 640;
-  vs.height = 480;
-  bpp = info->vfmt->BitsPerPixel;
-
-  SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 5);
-  SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 5);
-  SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 5);
-  SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 16);
-  SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
-
-  flags = SDL_OPENGL | SDL_RESIZABLE;
-
-  if (SDL_SetVideoMode(vs.width, vs.height, bpp, flags) == 0) {
-    fprintf(stderr, "ビデオモードのセットに失敗しました: %s\n", SDL_GetError());
-    SDL_Quit();
-    return 1;
-  }
-
-  setup_opengl(vs.width, vs.height);
-  SDL_WM_SetCaption(WM_TITLE, NULL);
-
-  /*struct MODEL* model;
-  model = model_mgr(1,0,NULL);
-  load_texture(model, "");
-  */
-  while (myflags.quit != 1) {
-    process_events();
-    draw_screen();
-  }
-
-  return 0;
-}
 
 static int setup_opengl(int width, int height) {
 
@@ -169,6 +151,78 @@ static int setup_opengl(int width, int height) {
     vs.scale = 15.0;
   }
 
+  return 0;
+}
+
+/*モデルデータを描画*/
+static int render_model(int num) {
+  int i, j;
+  int index, c;
+  struct MODEL *model;
+  struct DSP_MODEL *dsp_model;
+
+  static float *loc;
+  static float *nor;
+  static float *uv;
+  static struct DSP_MAT *mats;
+
+  model = model_mgr(1, num, NULL);
+  if (model == NULL)
+    return -1;
+  dsp_model = model_mgr(2, num, NULL);
+
+  loc = dsp_model->loc;
+  nor = dsp_model->nor;
+  uv = dsp_model->uv;
+  mats = dsp_model->mats;
+
+  if (loc == NULL)
+    return -1;
+  if (nor == NULL)
+    return -1;
+  if (uv == NULL)
+    return -1;
+  if (mats == NULL)
+    return -1;
+
+  c = 0;
+  for (i = 0; i < model->mat_count; i++) {
+    if (mats[i].texbits != NULL) {
+      glEnable(GL_TEXTURE_2D);
+      // glBindTexture(GL_TEXTURE_2D , dsp_model->texid[i]);
+
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+      glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+      glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+      struct DSP_MAT *mat = &mats[i];
+      glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, mats[i].texsize[0],
+                   mats[i].texsize[1], 0, GL_RGBA, GL_UNSIGNED_BYTE,
+                   mats[i].texbits);
+    }
+    glBegin(GL_TRIANGLES);
+    glColor4fv(mats[i].col);
+
+    for (j = 0; j < model->mat[i].vt_index_count; j++) {
+      index = model->vt_index[c++];
+      glTexCoord2fv(uv + 2 * index);
+      glVertex3fv(loc + 3 * index);
+      /*
+      glTexCoord2fv(model->vt[index].uv);
+      glVertex3fv(model->vt[index].loc);
+      */
+      // c++;
+    }
+
+    glEnd();
+    glDisable(GL_TEXTURE_2D);
+  }
+  /*
+  GLenum num;
+  num = glGetError;
+  */
+  // glDeleteTextures(model->mat_count, dsp_model->texid);
   return 0;
 }
 
@@ -344,132 +398,135 @@ static void process_events(void) {
   }
 }
 
-/*描画用のモデルを管理する関数*/
-void *model_mgr(int flag, int num, void *p) {
-  static struct MODEL model[16];
-  static struct DSP_MODEL dsp_model[16];
-  static int init = 1;
-
-  int i;
-
-  /*flag
-  -1:初期化
-  0:書き込み
-  1:読み出し
-  2:描画モデル読み出し
-  3:テクスチャはそのままで再セット
-  num
-  0:表示
-  */
-  if (flag == -1) {
-    for (i = 0; i < 16; i++) {
-      create_PMD(&model[i]);
-      make_dsp_model(&model[i], &dsp_model[i]);
-    }
-    init = 1;
-  } else if (flag == 0) {
-    init = -1;
-    delete_PMD(&model[num]);
-    copy_PMD(&model[num], (struct MODEL *)p);
-    make_dsp_model(&model[num], &dsp_model[num]);
-    init = 1;
-  } else if (flag == 1) {
-    // if(myflags.model_lock != 0)return NULL;
-    if (init == 1) {
-      load_tex(&model[num], &dsp_model[num]);
-      init = 0;
-    } else if (init == -1) {
-      return NULL;
-    }
-    return &model[num];
-
-  } else if (flag == 2) {
-    return &dsp_model[num];
-
-  } else if (flag == 3) {
-    init = -1;
-    delete_PMD(&model[num]);
-    copy_PMD(&model[num], (struct MODEL *)p);
-    make_dsp_model(&model[num], &dsp_model[num]);
-    init = 0;
+static int createwindow() {
+  if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+    fprintf(stderr, "ビデオの初期化に失敗しました: %s\n", SDL_GetError());
+    SDL_Quit();
+    return 1;
   }
+
+  info = SDL_GetVideoInfo();
+
+  if (!info) {
+    fprintf(stderr, "ビデオの問い合わせに失敗しました: %s\n", SDL_GetError());
+    SDL_Quit();
+    return 1;
+  }
+
+  vs.width = 640;
+  vs.height = 480;
+  bpp = info->vfmt->BitsPerPixel;
+
+  SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 5);
+  SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 5);
+  SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 5);
+  SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 16);
+  SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+
+  flags = SDL_OPENGL | SDL_RESIZABLE;
+
+  if (SDL_SetVideoMode(vs.width, vs.height, bpp, flags) == 0) {
+    fprintf(stderr, "ビデオモードのセットに失敗しました: %s\n", SDL_GetError());
+    SDL_Quit();
+    return 1;
+  }
+
+  setup_opengl(vs.width, vs.height);
+  SDL_WM_SetCaption(WM_TITLE, NULL);
+
+  /*struct MODEL* model;
+  model = model_mgr(1,0,NULL);
+  load_texture(model, "");
+  */
+  while (myflags.quit != 1) {
+    process_events();
+    draw_screen();
+  }
+
   return 0;
 }
 
-/*モデルデータを描画*/
-int render_model(int num) {
+// int (SDLCALL *fn)(void *)
+static int viewer_thread(void *) {
+  SDL_Event event;
+
+  myflags.model_lock = 0;
+
+  createwindow();
+  SDL_Quit();
+  return 0;
+}
+
+static int make_dsp_model(struct MODEL *model, struct DSP_MODEL *dsp_model) {
   int i, j;
-  int index, c;
-  struct MODEL *model;
-  struct DSP_MODEL *dsp_model;
+  float *loc;
+  float *nor;
+  float *uv;
+  struct DSP_MAT *mats;
+  GLuint *texid;
+  // unsigned int index;
 
-  static float *loc;
-  static float *nor;
-  static float *uv;
-  static struct DSP_MAT *mats;
-
-  model = model_mgr(1, num, NULL);
-  if (model == NULL)
-    return -1;
-  dsp_model = model_mgr(2, num, NULL);
-
-  loc = dsp_model->loc;
-  nor = dsp_model->nor;
-  uv = dsp_model->uv;
-  mats = dsp_model->mats;
-
-  if (loc == NULL)
-    return -1;
-  if (nor == NULL)
-    return -1;
-  if (uv == NULL)
-    return -1;
-  if (mats == NULL)
-    return -1;
-
-  c = 0;
-  for (i = 0; i < model->mat_count; i++) {
-    if (mats[i].texbits != NULL) {
-      glEnable(GL_TEXTURE_2D);
-      // glBindTexture(GL_TEXTURE_2D , dsp_model->texid[i]);
-
-      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-      glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-      glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-
-      struct DSP_MAT *mat = &mats[i];
-      glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, mats[i].texsize[0],
-                   mats[i].texsize[1], 0, GL_RGBA, GL_UNSIGNED_BYTE,
-                   mats[i].texbits);
-    }
-    glBegin(GL_TRIANGLES);
-    glColor4fv(mats[i].col);
-
-    for (j = 0; j < model->mat[i].vt_index_count; j++) {
-      index = model->vt_index[c++];
-      glTexCoord2fv(uv + 2 * index);
-      glVertex3fv(loc + 3 * index);
-      /*
-      glTexCoord2fv(model->vt[index].uv);
-      glVertex3fv(model->vt[index].loc);
-      */
-      // c++;
-    }
-
-    glEnd();
-    glDisable(GL_TEXTURE_2D);
-  }
   /*
-  GLenum num;
-  num = glGetError;
+  while(myflags.model_lock != 0){
+          SDL_Delay(30);
+  }
+  myflags.model_lock=1;
   */
-  // glDeleteTextures(model->mat_count, dsp_model->texid);
+  FREE(dsp_model->loc);
+  FREE(dsp_model->nor);
+  FREE(dsp_model->uv);
+  for (i = 0; i < dsp_model->mats_c; i++) {
+    FREE(dsp_model->mats[i].texbits);
+    dsp_model->mats[i].texbits = NULL;
+    memset(dsp_model->mats[i].texsize, 0, 2 * sizeof(int));
+  }
+  FREE(dsp_model->mats);
+  FREE(dsp_model->texid);
+  dsp_model->loc = NULL;
+  dsp_model->nor = NULL;
+  dsp_model->uv = NULL;
+  dsp_model->mats = NULL;
+  dsp_model->texid = NULL;
+
+  loc = MALLOC(model->vt_count * 3 * sizeof(float));
+  nor = MALLOC(model->vt_count * 3 * sizeof(float));
+  uv = MALLOC(model->vt_count * 2 * sizeof(float));
+  mats = MALLOC(model->mat_count * sizeof(struct DSP_MAT));
+  memset(mats, 0, model->mat_count * sizeof(struct DSP_MAT));
+  texid = MALLOC(model->mat_count * sizeof(GLuint));
+  if (loc == NULL || nor == NULL || uv == NULL || mats == NULL) {
+    // myflags.model_lock=0;
+    return -1;
+  }
+  dsp_model->loc = loc;
+  dsp_model->nor = nor;
+  dsp_model->uv = uv;
+  dsp_model->mats = mats;
+  dsp_model->texid = texid;
+  dsp_model->mats_c = model->mat_count;
+
+  for (i = 0; i < model->vt_count; i++) {
+    memcpy(loc, model->vt[i].loc, 2 * sizeof(float));
+    loc += 2;
+    *loc = -model->vt[i].loc[2];
+    memcpy(nor, model->vt[i].nor, 3 * sizeof(float));
+    memcpy(uv, model->vt[i].uv, 2 * sizeof(float));
+    loc++;
+    nor += 3;
+    uv += 2;
+  }
+
+  for (i = 0; i < dsp_model->mats_c; i++) {
+    dsp_model->mats[i].texbits = NULL;
+    memset(dsp_model->mats[i].texsize, 0, 2 * sizeof(int));
+  }
+
+  // myflags.model_lock=0;
   return 0;
 }
 
 // テクスチャ読み込み
-int load_tex(struct MODEL *model, struct DSP_MODEL *dsp_model) {
+static int load_tex(struct MODEL *model, struct DSP_MODEL *dsp_model) {
   struct DSP_MAT *mats;
   int i, j;
   /*
@@ -553,109 +610,56 @@ int load_tex(struct MODEL *model, struct DSP_MODEL *dsp_model) {
   return 0;
 }
 
-int make_dsp_model(struct MODEL *model, struct DSP_MODEL *dsp_model) {
-  int i, j;
-  float *loc;
-  float *nor;
-  float *uv;
-  struct DSP_MAT *mats;
-  GLuint *texid;
-  // unsigned int index;
+/*描画用のモデルを管理する関数*/
+void *model_mgr(int flag, int num, void *p) {
+  static struct MODEL model[16];
+  static struct DSP_MODEL dsp_model[16];
+  static int init = 1;
 
-  /*
-  while(myflags.model_lock != 0){
-          SDL_Delay(30);
-  }
-  myflags.model_lock=1;
+  int i;
+
+  /*flag
+  -1:初期化
+  0:書き込み
+  1:読み出し
+  2:描画モデル読み出し
+  3:テクスチャはそのままで再セット
+  num
+  0:表示
   */
-  FREE(dsp_model->loc);
-  FREE(dsp_model->nor);
-  FREE(dsp_model->uv);
-  for (i = 0; i < dsp_model->mats_c; i++) {
-    FREE(dsp_model->mats[i].texbits);
-    dsp_model->mats[i].texbits = NULL;
-    memset(dsp_model->mats[i].texsize, 0, 2 * sizeof(int));
-  }
-  FREE(dsp_model->mats);
-  FREE(dsp_model->texid);
-  dsp_model->loc = NULL;
-  dsp_model->nor = NULL;
-  dsp_model->uv = NULL;
-  dsp_model->mats = NULL;
-  dsp_model->texid = NULL;
+  if (flag == -1) {
+    for (i = 0; i < 16; i++) {
+      create_PMD(&model[i]);
+      make_dsp_model(&model[i], &dsp_model[i]);
+    }
+    init = 1;
+  } else if (flag == 0) {
+    init = -1;
+    delete_PMD(&model[num]);
+    copy_PMD(&model[num], (struct MODEL *)p);
+    make_dsp_model(&model[num], &dsp_model[num]);
+    init = 1;
+  } else if (flag == 1) {
+    // if(myflags.model_lock != 0)return NULL;
+    if (init == 1) {
+      load_tex(&model[num], &dsp_model[num]);
+      init = 0;
+    } else if (init == -1) {
+      return NULL;
+    }
+    return &model[num];
 
-  loc = MALLOC(model->vt_count * 3 * sizeof(float));
-  nor = MALLOC(model->vt_count * 3 * sizeof(float));
-  uv = MALLOC(model->vt_count * 2 * sizeof(float));
-  mats = MALLOC(model->mat_count * sizeof(struct DSP_MAT));
-  memset(mats, 0, model->mat_count * sizeof(struct DSP_MAT));
-  texid = MALLOC(model->mat_count * sizeof(GLuint));
-  if (loc == NULL || nor == NULL || uv == NULL || mats == NULL) {
-    // myflags.model_lock=0;
-    return -1;
-  }
-  dsp_model->loc = loc;
-  dsp_model->nor = nor;
-  dsp_model->uv = uv;
-  dsp_model->mats = mats;
-  dsp_model->texid = texid;
-  dsp_model->mats_c = model->mat_count;
+  } else if (flag == 2) {
+    return &dsp_model[num];
 
-  for (i = 0; i < model->vt_count; i++) {
-    memcpy(loc, model->vt[i].loc, 2 * sizeof(float));
-    loc += 2;
-    *loc = -model->vt[i].loc[2];
-    memcpy(nor, model->vt[i].nor, 3 * sizeof(float));
-    memcpy(uv, model->vt[i].uv, 2 * sizeof(float));
-    loc++;
-    nor += 3;
-    uv += 2;
+  } else if (flag == 3) {
+    init = -1;
+    delete_PMD(&model[num]);
+    copy_PMD(&model[num], (struct MODEL *)p);
+    make_dsp_model(&model[num], &dsp_model[num]);
+    init = 0;
   }
-
-  for (i = 0; i < dsp_model->mats_c; i++) {
-    dsp_model->mats[i].texbits = NULL;
-    memset(dsp_model->mats[i].texsize, 0, 2 * sizeof(int));
-  }
-
-  // myflags.model_lock=0;
   return 0;
-}
-
-/*
-** クォータニオンの積 r <- p x q
-*/
-void qmul(double r[], const double p[], const double q[]) {
-  r[0] = p[0] * q[0] - p[1] * q[1] - p[2] * q[2] - p[3] * q[3];
-  r[1] = p[0] * q[1] + p[1] * q[0] + p[2] * q[3] - p[3] * q[2];
-  r[2] = p[0] * q[2] - p[1] * q[3] + p[2] * q[0] + p[3] * q[1];
-  r[3] = p[0] * q[3] + p[1] * q[2] - p[2] * q[1] + p[3] * q[0];
-}
-
-/*
-** 回転の変換行列 r <- クォータニオン q
-*/
-void qrot(double r[], double q[]) {
-  double x2 = q[1] * q[1] * 2.0;
-  double y2 = q[2] * q[2] * 2.0;
-  double z2 = q[3] * q[3] * 2.0;
-  double xy = q[1] * q[2] * 2.0;
-  double yz = q[2] * q[3] * 2.0;
-  double zx = q[3] * q[1] * 2.0;
-  double xw = q[1] * q[0] * 2.0;
-  double yw = q[2] * q[0] * 2.0;
-  double zw = q[3] * q[0] * 2.0;
-
-  r[0] = 1.0 - y2 - z2;
-  r[1] = xy + zw;
-  r[2] = zx - yw;
-  r[4] = xy - zw;
-  r[5] = 1.0 - z2 - x2;
-  r[6] = yz + xw;
-  r[8] = zx + yw;
-  r[9] = yz - xw;
-  r[10] = 1.0 - x2 - y2;
-  r[3] = r[7] = r[11] = r[12] = r[13] = r[14] = 0.0;
-  r[15] = 1.0;
 }
 
 void CreateViewerThread() {
